854d2a9f30328e40449bc1306e20d73b64a3c87e
AndrWan1 <148342863+AndrWan1@users.noreply.github.com>
2024-02-18 19:45:27 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,32 @@
+// const array = ['mango', 'poly', 'ajax']
+
+// for (let i = 0; i < array.length; i += 1) {
+//    console.log(i, array[i]);
+// }
+
+
+
+// function minNum(numbers){
+//     let smallNumber = numbers[0];
+//     for (const number of numbers) {
+//         if (smallNumber > number) {
+//             smallNumber = number;
+            
+//         }
+//     }
+//     return smallNumber;
+// }
+
+// console.log(minNum([26, 10, 8, 20, 95, 67, 77, 5, 45]));
+
+
+
+// const str = "ротатор"
+// const newString = "Hello"
+
+// const strReversed = str.split('').reverse('').join('');
+// // console.log(strReversed === str);
+
+
+// const newStringReverse = newString.split('').reverse('').join('');
+// console.log(newStringReverse === newString);
